3ed3ee0c51f58e06e224c8368813904fb36150f1
theraffael <raffael.kummer@uzh.ch>
2021-09-28 12:29:47 +0200

    Implemented "The Twelve Days of Christmas"

--- src/com/example/SoftCon/AssignmentZero.java
+++ src/com/example/SoftCon/AssignmentZero.java
@@ -2,6 +2,103 @@ package com.example.SoftCon;
 
 public class AssignmentZero {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String Day1 = "On the first day of Christmas, my true love sent to me\n A partridge in a pear tree";
+        String Day2 = "On the second day of Christmas, my true love sent to me\n" + "Two turtledoves\n" + "And a partridge in a pear tree";
+        String Day3 = "On the third day of Christmas, my true love sent to me\n" + "Three French hens\n" + "Two turtledoves\n" + "And a partridge in a pear tree";
+        String Day4to12 = "On the fourth day of Christmas, my true love sent to me\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "\n" +
+                "On the fifth day of Christmas, my true love sent to me\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "On the sixth day of Christmas, my true love sent to me\n" +
+                "Six geese a-laying\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "On the seventh day of Christmas, my true love sent to me\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "On the eighth day of Christmas, my true love sent to me\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "On the ninth day of Christmas, my true love sent to me\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "On the tenth day of Christmas, my true love sent to me\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "On the eleventh day of Christmas, my true love sent to me\n" +
+                "I sent eleven pipers piping\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "\n" +
+                "On the twelfth day of Christmas, my true love sent to me\n" +
+                "Twelve drummers drumming\n" +
+                "Eleven pipers piping\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five gold rings (five golden rings)\n" +
+                "Four calling birds\n" +
+                "Three French hens\n" +
+                "Two turtledoves\n" +
+                "And a partridge in a pear tree\n" +
+                "And a partridge in a pear tree";
+        System.out.println(Day1 + "\n\n" +
+                            Day2 + "\n\n" +
+                            Day3+ "\n\n" +
+                            Day4to12);
     }
 }
