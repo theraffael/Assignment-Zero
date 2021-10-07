5e7fe035b524cd124a4e7ef6aab14149561c98dc
Dylan <dylanbaumgartner@hotmail.com>
2021-10-07 17:44:21 +0200

    (refactor) changed file name

---
+++ Assignment1/src/model/Board.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Board {
+    String  letterCoordinates = "a b c d e f g h";
+    String  horizontalBoarder = "+-------------------------------------------------+";
+
+    public void display()
+    {
+        System.out.println(horizontalBoarder);
+    }
+}
