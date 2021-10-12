c8a3544d8b692b9a51858e606f259a49d9c1bd58
theraffael <raffael.kummer@uzh.ch>
2021-10-13 00:05:07 +0200

    added some methods to the game class (no content yet)

--- Assignment1/src/logic/Game.java
+++ Assignment1/src/logic/Game.java
@@ -2,5 +2,33 @@ package logic;
 
 
 public class Game {
+    //starts a new game
+    public void newGame(){
 
+    }
+
+    //executes a move
+    public void newMove(){
+
+    }
+
+    //checks if move is legal
+    public void isLegal(){
+
+    }
+
+    //checks whether the game is finished
+    public void isFinished(){
+
+    }
+
+    //returns the number of the player whose turn it is
+    public void playersTurn(){
+
+    }
+
+    //calculates all possible moves
+    public void CalcPossibleMoves(){
+
+    }
 }
