1627af5d20852ac1cab2d4412d4800dc0877ebc4
Dylan <dylanbaumgartner@hotmail.com>
2021-10-15 18:45:46 +0200

    added input handling

--- Assignment1/src/CheckerGame.java
+++ Assignment1/src/CheckerGame.java
@@ -21,15 +21,41 @@ public class CheckerGame {
     public static void runGame(){
         Game game = new Game(board, redPlayer, whitePlayer);
         while (!game.isFinished()){
+            boolean isInputCorrect = false;
+            String move = null;
             board.display();
             System.out.println("Player Turn: "+ game.getActivePlayer().getColorWord());
-            String move = keyBoard.nextLine();
-            boolean isInputCorrect = checkInputIsValid(move);
+            while(!isInputCorrect){
+                System.out.println("Please enter coordinate format in form of [a1]x[b2]");
+                move = keyBoard.nextLine();
+                isInputCorrect = checkInputIsValid(move);
+                if (isInputCorrect){
+                    break;
+                }
+                else{
+                    System.out.println("Coordinate format is incorrect, please try again");
+                }
+            }
+            String convertedMove = convertInputToXY(move);
             // move red pawn at D3 to E4, no logic implemented yet
-            game.newMove(3,2,4,3);
+            game.newMove(convertedMove.charAt(0),convertedMove.charAt(1),convertedMove.charAt(2),convertedMove.charAt(3));
         }
     }
 
+    public static String convertInputToXY(String s){
+        s = s.replaceAll("[\\[x\\]]","").toLowerCase(Locale.ROOT);
+        String newstring = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isAlphabetic(ch)) {
+                int pos = ch - 'a' + 1;
+                newstring = newstring + pos;
+            } else {
+                newstring = newstring + ch;
+            }
+        }
+        return newstring;
+    }
     public static boolean checkInputIsValid(String input){
         Pattern p = Pattern.compile("\\[[a-z][1-8]\\]x\\[[a-z][1-8]\\]");
         String lowercase = input.toLowerCase(Locale.ROOT);
