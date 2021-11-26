7c60a2fae4b1de7d63c438ac597dbd79bf9922a3
theraffael <raffael.kummer@uzh.ch>
2021-11-26 12:36:11 +0100

    some minor improvements

--- Assignment2/test/CheckerGameTest.java
+++ Assignment2/test/CheckerGameTest.java
@@ -11,10 +11,13 @@ import java.util.Random;
 public class CheckerGameTest {
 
     @Test
-    void MinMaxVsRandom(){ //todo: suppress output of game.runGame()
+    void MinMaxVsRandom(){
+        int runs = 10;
+        int wonByMinMay = 0;
+        System.out.println("\n----------\nBEHOLD! In the following test you shall bear witness to not one but "+runs+" matches between the great MinMax-Player and the not-so-great Random Player!\n----------\n");
 
 
-        for (int k = 0; k<10 ; k++){
+        for (int i = 0; i< runs ; i++){
             Board board = new Board(true);
             PlayerContext redPlayer = new PlayerContext(new RandomPlayer(), PlayerColor.RED);
             PlayerContext whitePlayer = new PlayerContext(new MinMaxPlayer(), PlayerColor.WHITE);
@@ -26,10 +29,17 @@ public class CheckerGameTest {
             game.runGame();
             //System.out.println(board.getBoardString());
 
-            System.out.println("Run number " + k + " was lost by: " + game.getActivePlayer().toString());
+            System.out.println("Run number " + i + " was lost by: " + game.getActivePlayer().toString());
+            if (game.isFinished() && (game.getActivePlayer() == PlayerColor.RED)){
+                wonByMinMay++;
+            }
             assertTrue(game.isFinished());
             assertEquals(PlayerColor.RED, game.getActivePlayer());
         }
+        assertTrue(wonByMinMay> (runs-1)); //sometimes the random player wins, which is expected although unlikely
+
+        System.out.println("\n--------------\n" +
+                "The MinMax Player won "+wonByMinMay+" out of "+ runs +" times (9 times needed to pass).\n--------------\n");
 
     }
 }
