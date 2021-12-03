9cd7a16f84428ce29cc2093eb56ddda0944cb05b
Lukas <fischerl@cl.uzh.ch>
2021-12-03 09:32:23 +0100

    sample calls to deck to show functionality

--- Assignment4/src/model/Game.java
+++ Assignment4/src/model/Game.java
@@ -7,10 +7,12 @@ public class Game {
     boolean continueGame;
     private Dealer dealer;
     private Player player;
+    private Deck deck;
 
     public Game(){
        this.dealer = new Dealer();
        this.player = new Player(100, "placeHolder");
+       this.deck = new Deck(1);
     }
 
     /**
@@ -19,6 +21,16 @@ public class Game {
     public void runGame(){
         //UI call Game introduction
         while (continueGame){
+            // shuffle deck halfway through to avoid running out of cards and so players cannot count cards ;)
+            if (deck.percentageOfCardsLeft() <= 50) {
+                deck.shuffleDeck();
+                // todo: UI call to let player know that deck was shuffled
+            }
+
+            // how to draw cards:
+            if (deck.hasNext()) {
+                Card card = deck.next();
+            }
             
         }
     }
