f9e02cb2ba3f765c4ea9895035c94c98676c38b4
Dylan Baumgartner <djb.21@live.com>
2021-12-03 14:03:05 +0100

    merged changes

--- Assignment4/src/model/Game.java
+++ Assignment4/src/model/Game.java
@@ -7,15 +7,16 @@ public class Game {
      * Boolean value if Player wants to continue Game
      */
     boolean continueGame;
+    boolean continueRound;
     private Dealer dealer;
     private Deck deck;
     private ArrayList<Player> playerList;
-    private int AgentCount;
+    private int agentCount;
 
     public Game(){
        this.dealer = new Dealer();
        playerList.add(new Player(100, "placeHolder"));
-       this.AgentCount = playerList.size() + 1;
+       this.agentCount = playerList.size() + 1;
        this.deck = new Deck();
        deck.shuffleDeck();
     }
@@ -25,27 +26,46 @@ public class Game {
      */
     public void runGame(){
         //UI call Game introduction
+
         while (continueGame){
-            this.beginningRoundCardDraw();
+
             if (deck.percentageOfCardsLeft() <= 50) {
                 deck.shuffleDeck();
                 // todo: UI call to let player know that deck was shuffled
             }
-            for(Player player : playerList){
-                Call playerCall = player.hitOrStay();
-                if(playerCall == Call.HIT){
-                    player.addCardToHand(deck.next());
+
+            int stayCount = 0;
+            continueRound = true;
+            while (continueRound){
+                //beginning card draw
+                this.beginningRoundCardDraw();
+
+                //iterate over playerList option for more players
+                for(Player player : playerList){
+                    //ask player if they are hitting or staying
+                    Call playerCall = player.hitOrStay();
+                    if(playerCall == Call.HIT){
+                        player.addCardToHand(deck.next());
+                    }
+                    else{
+                        stayCount++;
+                    }
+                }
+
+                //dealer hits if less than 17 points
+                Call dealerCall = dealer.hitOrStay();
+                if(dealerCall == Call.HIT){
+                    dealer.addCardToHand(deck.next());
                 }
                 else{
+                    stayCount++;
+                }
 
+                //if amount of stays are equal to agents on table stop round
+                if(stayCount == this.agentCount){
+                    continueRound = false;
                 }
             }
-            Call dealerCall = dealer.hitOrStay();
-            if(dealerCall == Call.HIT){
-                dealer.addCardToHand(deck.next());
-            }
-
-
         }
     }
 
